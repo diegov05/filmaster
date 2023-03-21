4bcd169bdd7cd4fb031b9ba14f912359e09fba89
Diego Vargas <diegovs0504@gmail.com>
2023-03-21 14:29:34 -0400

    Basic App Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Header, Catalog, CatalogList, Footer } from './containers'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <Catalog title='Movies' data={[]} />
+      <Catalog title='Series' data={[]} />
+      <CatalogList />
+      <Footer />
+    </>
   )
 }
 
