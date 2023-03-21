ad0e64af004ddca9af3fa032197a697897c9ac5d
Diego Vargas <diegovs0504@gmail.com>
2023-03-21 14:12:23 -0400

    Created Components Files

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./MovieCard.css"
+
+export default function MovieCard() {
+    return (
+        <div>MovieCard</div>
+    )
+}
---
+++ src/components/MovieItem/MovieItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./MovieItem.css"
+export default function MovieItem() {
+    return (
+        <div>MovieItem</div>
+    )
+}
