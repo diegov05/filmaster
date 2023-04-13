9e04d2e154d607ef099fa54d040c711804ccbd9b
Diego Vargas <diegovs0504@gmail.com>
2023-04-13 14:50:04 -0400

    Added Firebase to MainPage

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { FC, useState } from 'react'
 import { Header, Catalog, CatalogList, Footer } from './containers'
 import { NavBar } from './components'
+import { getAuth, signOut } from 'firebase/auth'
 
-function MainPage() {
+interface IHomePageProps { }
+
+export const MainPage: React.FunctionComponent<IHomePageProps> = (props) => {
+    const auth = getAuth()
 
     return (
         <>
-            <NavBar />
+            <NavBar authed={auth.currentUser?.isAnonymous ? false : true} />
             <Header />
             <Catalog title='Movies' identifier="movies" />
             <Catalog title='Series' identifier="series" />
@@ -16,4 +20,4 @@ function MainPage() {
     )
 }
 
-export { MainPage }
+
