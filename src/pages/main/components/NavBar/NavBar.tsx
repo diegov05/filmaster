df1fb426d951c8cc2f39aa22a245bd5a93f2b9f1
Diego Vargas <diegovs0504@gmail.com>
2023-04-13 14:50:48 -0400

    Major changes in Navbar
    * Transcribed ALL CSS from Vanilla to Tailwind.
    * Implemented SignOut function

--- src/pages/main/components/NavBar/NavBar.tsx
+++ src/pages/main/components/NavBar/NavBar.tsx
@@ -1,25 +1,83 @@
-import React, { FC } from 'react'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import React, { FC, useState } from 'react'
+import { MagnifyingGlassIcon, UserIcon, ChevronDownIcon, ChevronUpIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 import { images } from '../../constants'
 import "./NavBar.css"
+import { getAuth, signOut } from 'firebase/auth'
 
-export const NavBar: FC = () => {
+interface INavBarProps {
+    authed: boolean
+}
+
+export const NavBar: FC<INavBarProps> = (props) => {
+
+    const list: Array<Object> = [
+        {
+            option: "Account"
+        },
+        {
+            option: "Profile"
+        },
+        {
+            option: "TMDB"
+        },
+        {
+            option: "Sign Out",
+            onClick: () => signOut(auth)
+        },
+    ]
+
+    const auth = getAuth()
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <div className='app__wrapper-navbar'>
-            <div className='app__wrapper-navbar_logo'>
-                <a href="#"><img src={images.logo} alt="logo" /></a>
-            </div>
-            <div className='app__wrapper-navbar_user'>
-                <form action="" className="relative mx-auto w-max">
-                    <input type="search"
-                        className=" peer cursor-pointer relative z-10 h-12 w-12 border-white bg-transparent pl-8 outline-none focus:w-full focus:cursor-text focus:border-white focus:pl-16 focus:pr-4 focus:border-b focus:border-solid text-white" />
-                    <MagnifyingGlassIcon className='absolute inset-y-0 my-auto custom__icon magnifying__icon' />
-                </form>
-                <a href="#" className='paragraph'>Sign In</a>
-                <div className='app__wrapper-navbar_user-register'>
-                    <button className='custom__button text-white font-semibold hover:opacity-90 hover:bg-purple-800 hover:border-transparent'>Register</button>
+
+        <div className='flex p-4 flex-col w-full h-80 justify-between absolute z-10'>
+            <div className='flex flex-row justify-between items-center'>
+                <div className='flex flex-col items-center justify-center'>
+                    <a href="#"><img src={images.logo} alt="logo" className='w-36' /></a>
+                </div>
+                <div className='flex flex-row items-center justify-center'>
+                    <form action="" className="relative mx-auto w-max">
+                        <input type="search"
+                            className=" peer cursor-pointer relative z-10 h-12 w-12 border-white bg-transparent pl-8 outline-none focus:w-full focus:cursor-text focus:border-white focus:pl-16 focus:pr-4 focus:border-b focus:border-solid text-white" />
+                        <MagnifyingGlassIcon className='absolute inset-y-0 my-auto custom__icon magnifying__icon' />
+                    </form>
+                    {props.authed ?
+                        (
+                            <>
+                                <button onClick={() => setIsOpen((prevState) => !prevState)} className='flex flex-row cursor-pointer justify-between items-center gap-4 bg-white px-4 py-2 rounded-xl hover:opacity-90 transition-all'>
+                                    <UserIcon className='custom__icon text-zinc-600' />
+                                    {!isOpen ? (
+                                        <ChevronDownIcon className='custom__icon text-zinc-600' />
+
+                                    ) : (
+                                        <ChevronUpIcon className='custom__icon text-zinc-600' />
+
+                                    )}
+                                </button>
+
+                                {isOpen && (
+                                    <div className='dropdown bg-white absolute top-20 right-1 flex flex-col items-center cursor-pointer rounded-xl'>
+                                        {list.map((item: any, i) => (
+                                            <button className='transition-all hover:bg-violet-900 p-4 w-full flex flex-col justify-center items-center border-b' key={i} onClick={item.onClick}>
+                                                <h2 className='transition-all paragraph text-zinc-600'>{item.option}</h2>
+                                                <span>{item.icon}</span>
+                                            </button>
+                                        ))}
+                                    </div>
+                                )}
+                            </>)
+                        : (
+                            <>
+                                <a href="#" className='paragraph'>Sign In</a>
+                                <div className='app__wrapper-navbar_user-register'>
+                                    <button className='custom__button text-white font-semibold hover:opacity-90 hover:bg-purple-800 hover:border-transparent'>Register</button>
+                                </div>
+                            </>)}
+
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
