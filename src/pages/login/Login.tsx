f098894af86d4228f2259aa622520d65d4d4d82b
Diego Vargas <diegovs0504@gmail.com>
2023-04-13 15:16:01 -0400

    Added Sign In as Anonymous Functionality

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import images from "../../assets"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInAnonymously, signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
 import "./Login.css"
@@ -26,6 +26,21 @@ export const Login = () => {
             })
     }
 
+    // const signInAnonymously = async () => {
+    //     setAuthing(true)
+
+    //     signInAnonymously()
+    //         .then(() => {
+    //             console.log("anonymous user logged in.")
+    //             navigate('/')
+    //         })
+    //         .catch(error => {
+    //             console.log(error)
+    //             setAuthing(false)
+    //         })
+
+    // }
+
     return (
 
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
@@ -40,7 +55,10 @@ export const Login = () => {
                         <button disabled={authing} className='w-full rounded-none bg-violet-900 outline-none border-none py-4 px-4 uppercase text-white custom__button hover:opacity-90'>Log In</button>
                         <div className='w-full flex flex-row justify-between gap-2'>
                             <button onClick={() => signInWithGoogle()} disabled={authing} className='flex flex-row justify-between items-center w-full rounded-none bg-zinc-900 outline-none border-none py-2 px-4 uppercase text-xs text-zinc-500 custom__button hover:opacity-90 hover:bg-white hover:text-violet-900'><FcGoogle />Log In With Google</button>
-                            <button onClick={() => { }} disabled={authing} className='w-full rounded-none bg-zinc-900 outline-none border-none py-2 px-4 uppercase text-xs text-zinc-500 custom__button hover:opacity-90'>Guest Login</button>
+                            <button onClick={() => {
+                                signInAnonymously(auth)
+                                navigate('/')
+                            }} disabled={authing} className='w-full rounded-none bg-zinc-900 outline-none border-none py-2 px-4 uppercase text-xs text-zinc-500 custom__button hover:opacity-90'>Guest Login</button>
                         </div>
                         <a href=""><p className='paragraph underline text-xs transition-all hover:opacity-90'>Forgot your password?</p></a>
                         <div className='flex flex-row gap-4 w-full mt-8 justify-center items-center'>
