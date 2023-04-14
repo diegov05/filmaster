d8fbf4b8dc3aa0f1f499997a1c3b3c43cc80493d
Diego Vargas <diegovs0504@gmail.com>
2023-04-14 17:22:33 -0400

    Exported User Page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,11 @@
 import { MainPage } from "./main/MainPage";
 import { Login } from "./login/Login";
 import { SignUp } from "./signup/SignUp";
+import { User } from "./user/User";
 
 export {
     MainPage,
     Login,
-    SignUp
+    SignUp,
+    User
 }
