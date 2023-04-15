a8c7bbcac7a34bbb19e47636306396b7bd9fed16
Diego Vargas <diegovs0504@gmail.com>
2023-04-15 14:38:05 -0400

    Created Account Component

---
+++ src/pages/user/components/Account/Account.tsx
@@ -0,0 +1,24 @@
+import { getAuth } from 'firebase/auth';
+import React from 'react';
+
+export type IAccountProps = {
+
+}
+
+const Account: React.FC<IAccountProps> = ({ }) => {
+
+    const auth = getAuth()
+
+    return (
+        <div className='flex flex-row gap-4 justify-center items-center'>
+            <div className='flex flex-col justify-center items-center'>
+                {auth.currentUser?.photoURL ? <img src={`${auth.currentUser?.photoURL!}`} className='w-16 rounded-full' alt="avatar" /> : <button className='custom__button paragraph uppercase'>Upload an Image</button>}
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+                <p className='paragraph font-bold'>{auth.currentUser?.email}</p>
+            </div>
+        </div>
+    );
+}
+
+export { Account };
