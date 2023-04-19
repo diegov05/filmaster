7d1a86352e280f254bb4fce3155b820544045234
Diego Vargas <diegovs0504@gmail.com>
2023-04-19 16:27:41 -0400

    Update Account.tsx

--- src/pages/user/components/Account/Account.tsx
+++ src/pages/user/components/Account/Account.tsx
@@ -1,5 +1,5 @@
-import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../../../App';
 
 export type IAccountProps = {
 
@@ -7,12 +7,11 @@ export type IAccountProps = {
 
 const Account: React.FC<IAccountProps> = ({ }) => {
 
-    const auth = getAuth()
 
     return (
         <div className='flex flex-row gap-4 justify-center items-center'>
             <div className='flex flex-col justify-center items-center'>
-                {auth.currentUser?.photoURL ? <img src={`${auth.currentUser?.photoURL!}`} className='w-16 rounded-full' alt="avatar" /> : <button className='custom__button paragraph uppercase'>Upload an Image</button>}
+                {auth.currentUser?.photoURL ? <img src={`${auth.currentUser?.photoURL}`} className='w-16 rounded-full' alt="avatar" /> : <button className='custom__button paragraph uppercase'>Upload an Image</button>}
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <p className='paragraph font-bold'>{auth.currentUser?.email}</p>
