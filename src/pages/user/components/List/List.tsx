be6da659bd68da56de30ab39b2b042432cd75fa3
Diego Vargas <diegovs0504@gmail.com>
2023-04-19 16:26:48 -0400

    Implemented New Way of Signing Out in User Page

--- src/pages/user/components/List/List.tsx
+++ src/pages/user/components/List/List.tsx
@@ -2,6 +2,7 @@ import { getAuth, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Account } from '../Account/Account';
+import { Favorites } from '../Favorites/Favorites';
 import './List.css'
 
 const List: React.FC = () => {
@@ -26,20 +27,32 @@ const List: React.FC = () => {
                 <li onClick={() => handleListItemClick(2)} className={`${selectedIdx === 2 ? 'selected' : ''} w-full cursor-pointer transition-all hover:bg-violet-900 hover:font-black text-white text-xl py-8 px-8 uppercase`}>
                     Favorites
                 </li>
-                <li onClick={() => {
-                    signOut(auth)
-                    navigate('/login')
-                }} className={`${selectedIdx === 3 ? 'selected' : ''} w-full cursor-pointer transition-all hover:bg-violet-900 hover:font-black text-white text-xl py-8 px-8 uppercase`}>
+                <li onClick={() => handleListItemClick(3)} className={`${selectedIdx === 3 ? 'selected' : ''} w-full cursor-pointer transition-all hover:bg-violet-900 hover:font-black text-white text-xl py-8 px-8 uppercase`}>
                     Sign Out
                 </li>
             </ul>
             <div>
                 {selectedIdx === 0 && <Account />}
                 {selectedIdx === 1 && <Account />}
-                {selectedIdx === 2 && <div className='text-white'>Option 3 content</div>}
-                {selectedIdx === 3 && <div className='text-white'>Option 4 content</div>}
+                {selectedIdx === 2 && <Favorites />}
+                {selectedIdx === 3 && (
+                    <div className='flex flex-col justify-start items-start gap-2'>
+                        <h1 className='headtext uppercase'>Are you sure you want to sign out?</h1>
+                        <div className='flex flex-row gap-2'>
+                            <button
+                                onClick={() => setSelectedIdx(0)}
+                                className='custom__button headtext rounded-none uppercase border-0 outline-0 bg-zinc-700 px-6 py-3'>Cancel</button>
+                            <button
+                                onClick={() => {
+                                    signOut(auth)
+                                    navigate('/login')
+                                }}
+                                className='custom__button headtext rounded-none uppercase border-0 outline-0 bg-violet-900 px-6 py-3'>Sign Out</button>
+                        </div>
+                    </div>
+                )}
             </div>
-        </div>
+        </div >
     );
 };
 
