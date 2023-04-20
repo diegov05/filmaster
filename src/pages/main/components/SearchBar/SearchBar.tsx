40687b2ac43be52e3491ebe1f295e670fdf05bfe
Diego Vargas <diegovs0504@gmail.com>
2023-04-20 11:47:41 -0400

    Finished SearchBar

--- src/pages/main/components/SearchBar/SearchBar.tsx
+++ src/pages/main/components/SearchBar/SearchBar.tsx
@@ -25,6 +25,7 @@ interface Movie {
     imdb_id: string | null;
     original_language: string;
     original_title: string;
+    name: string;
     overview: string | null;
     popularity: number;
     poster_path: string | null;
@@ -39,6 +40,8 @@ interface Movie {
         name: string;
     }[];
     release_date: string;
+    last_air_date: string;
+    media_type: string;
     revenue: number;
     runtime: number | null;
     spoken_languages: {
@@ -63,14 +66,19 @@ export const SearchBar = () => {
 
         if (query.length > 0) {
             const response = await axios.get(
-                `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`
+                `https://api.themoviedb.org/3/search/multi?api_key=${key}&query=${query}`
             );
 
-            setResults(response.data.results);
+            const moviesAndSeries = response.data.results.filter(
+                (result: any) => result.media_type === 'movie' || result.media_type === 'tv'
+            );
+
+            setResults(moviesAndSeries);
         } else {
             setResults([]);
         }
     }
+
 
     return (
         <div>
@@ -81,19 +89,21 @@ export const SearchBar = () => {
             {results.length > 0 && (
                 <div className='scrollbar mt-2 bg-zinc-800 rounded-xl first-letter: h-96 w-full z-50 absolute'>
                     {results.map((movie) => (
-                        <div className='p-4 flex flex-row justify-start items-start gap-4 border-b border-zinc-600' key={movie.id}>
+                        <div className='p-4 flex flex-row justify-start items-start gap-4 border-b border-zinc-600 hover:bg-black cursor-pointer transition-all' key={movie.id}>
                             <img className='w-12' src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`} alt={movie.title} />
                             <div className='flex flex-col gap-2'>
-                                <p className='paragraph text-xs max-w-[12ch]'>{movie.title}</p>
+                                <p className='paragraph text-xs max-w-[12ch]'>{movie.title ? movie.title : movie.name}</p>
                                 <div className='flex flex-row justify-start items-start gap-1'>
-                                    <button className='headtext py-0 px-1 text-[.5rem] custom__button bg-green-400 border-0 outline-0 text-black'>NEW</button>
-                                    <button className='headtext py-0 px-1 text-[.5rem] custom__button bg-amber-400 border-0 outline-0 text-black'>POPULAR</button>
+                                    {movie.release_date ? (parseInt(movie.release_date.slice(0, 4)) > 2021 ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-green-400 border-0 outline-0 text-black'>NEW</div> : "") : movie.media_type !== "Movie" ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-green-400 border-0 outline-0 text-black'>TV</div> : ""}
+                                    {movie.vote_count > 1000 ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-amber-400 border-0 outline-0 text-black'>POPULAR</div> : ""}
+
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
-            )}
-        </div>
+            )
+            }
+        </div >
     );
 }
