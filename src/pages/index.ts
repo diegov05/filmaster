de92e654bac914488a2b0b866182b0ef1dd7250f
Diego Vargas <diegovs0504@gmail.com>
2023-04-21 12:35:52 -0400

    Movie Page Export

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,10 +2,12 @@ import { MainPage } from "./main/MainPage";
 import { Login } from "./login/Login";
 import { SignUp } from "./signup/SignUp";
 import { User } from "./user/User";
+import { Movie } from "./movie/Movie";
 
 export {
     MainPage,
     Login,
     SignUp,
-    User
+    User,
+    Movie
 }
