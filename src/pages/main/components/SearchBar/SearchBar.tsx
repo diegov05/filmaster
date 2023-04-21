d6fb8268f27ff0a40d0f53e25ca42b0c57021837
Diego Vargas <diegovs0504@gmail.com>
2023-04-21 12:35:39 -0400

    Added WatchProviders to Movie Interface

--- src/pages/main/components/SearchBar/SearchBar.tsx
+++ src/pages/main/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { MouseEventHandler, ReactElement, useState } from 'react';
 import axios from 'axios';
 import { key } from '../../constants/requests';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { StarRating } from '../StarRating/StarRating';
+import { useNavigate } from 'react-router-dom'
+
+import { Provider } from '../../../movie/Movie';
 
 import './SearchBar.css'
+import { MovieResult } from './MovieResult';
 
-interface Movie {
+export interface Movie {
     adult: boolean;
     backdrop_path: string | null;
     belongs_to_collection: {
@@ -54,9 +58,18 @@ interface Movie {
     video: boolean;
     vote_average: number;
     vote_count: number;
+    watchProviders: {
+        [countryCode: string]: {
+            link: string;
+            flatrate: Provider[];
+            rent: Provider[];
+            buy: Provider[];
+        };
+    };
 }
 
 export const SearchBar = () => {
+    const navigate = useNavigate()
     const [query, setQuery] = useState('');
     const [results, setResults] = useState<Movie[]>([]);
 
@@ -79,28 +92,20 @@ export const SearchBar = () => {
         }
     }
 
+    const handleClick = (movieId: number) => {
+        navigate(`/movie/${movieId}`)
+    }
 
     return (
         <div>
             <input type="search"
-                className=" peer cursor-pointer relative z-10 h-12 w-12 border-white bg-transparent pl-8 outline-none focus:w-full focus:cursor-text focus:border-white focus:pl-16 focus:pr-4 focus:border-b focus:border-solid text-white" value={query} onChange={handleSearch} />
+                className=" peer cursor-pointer relative z-50 h-12 w-12 border-white bg-transparent pl-8 outline-none focus:w-full focus:cursor-text focus:border-white focus:pl-16 focus:pr-4 focus:border-b focus:border-solid text-white" value={query} onChange={handleSearch} />
             <MagnifyingGlassIcon className='absolute inset-y-0 my-auto custom__icon magnifying__icon' />
 
             {results.length > 0 && (
                 <div className='scrollbar mt-2 bg-zinc-800 rounded-xl first-letter: h-96 w-full z-50 absolute'>
-                    {results.map((movie) => (
-                        <div className='p-4 flex flex-row justify-start items-start gap-4 border-b border-zinc-600 hover:bg-black cursor-pointer transition-all' key={movie.id}>
-                            <img className='w-12' src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`} alt={movie.title} />
-                            <div className='flex flex-col gap-2'>
-                                <p className='paragraph text-xs max-w-[12ch]'>{movie.title ? movie.title : movie.name}</p>
-                                <div className='flex flex-row justify-start items-start gap-1'>
-                                    {movie.release_date ? (parseInt(movie.release_date.slice(0, 4)) > 2021 ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-green-400 border-0 outline-0 text-black'>NEW</div> : "") : movie.media_type !== "Movie" ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-green-400 border-0 outline-0 text-black'>TV</div> : ""}
-                                    {movie.vote_count > 1000 ? <div className='headtext py-0 px-1 text-[.5rem] custom__button bg-amber-400 border-0 outline-0 text-black'>POPULAR</div> : ""}
-
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                    {results.map((movie) => < MovieResult movie={movie
+                    } />)}
                 </div>
             )
             }
