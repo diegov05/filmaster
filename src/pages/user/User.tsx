8939afd5b50149739dbcdc2e7f5d5e26ef70aee2
Diego Vargas <diegovs0504@gmail.com>
2023-04-23 19:44:09 -0400

    Changed Logo and Text sizes in User Page

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -16,10 +16,10 @@ const User: React.FC<IUserProps> = (props) => {
     return (
         <div>
             <div className='p-4 flex flex-row justify-between items-start'>
-                <img className="w-80 cursor-pointer" src={images.logo} alt="logo" onClick={() => { navigate('/') }} />
+                <img className="w-36 cursor-pointer" src={images.logo} alt="logo" onClick={() => { navigate('/') }} />
                 <div className='flex flex-col gap-4 justify-start items-start'>
-                    <button className='custom__button headtext uppercase rounded-none px-6 py-3 bg-violet-900 hover:bg-white hover:text-violet-900 border-none outline-none' onClick={() => { navigate('/') }}>Return to main page</button>
-                    <p className='text-sm text-white paragraph font-light'>Logged In As:
+                    <button className='custom__button text-xs headtext uppercase rounded-none px-3 py-1 bg-violet-900 hover:bg-white hover:text-violet-900 border-none outline-none' onClick={() => { navigate('/') }}>Return to main page</button>
+                    <p className='text-xs text-white paragraph font-light'>Logged In As:
                         <br /> {props.user?.email}</p>
                 </div>
             </div>
