076c043a8702bd0d6e4285d237e4013bca32451a
Diego Vargas <diegovs0504@gmail.com>
2023-04-25 20:00:43 -0400

    Added NavBar Component to User Page

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,28 +1,13 @@
-import { User, getAuth } from 'firebase/auth';
 import React from 'react';
-import { images } from '../main/constants';
-import { useNavigate } from 'react-router-dom';
-import { List } from './components';
+import { List, NavBar } from './components';
 
 
-export interface IUserProps {
-    user: User
-}
+export interface IUserProps { }
 
 const User: React.FC<IUserProps> = (props) => {
-
-    const navigate = useNavigate()
-
     return (
         <div>
-            <div className='p-4 flex flex-row justify-between items-start'>
-                <img className="w-36 cursor-pointer" src={images.logo} alt="logo" onClick={() => { navigate('/') }} />
-                <div className='flex flex-col gap-4 justify-start items-start'>
-                    <button className='custom__button text-xs headtext uppercase rounded-none px-3 py-1 bg-violet-900 hover:bg-white hover:text-violet-900 border-none outline-none' onClick={() => { navigate('/') }}>Return to main page</button>
-                    <p className='text-xs text-white paragraph font-light'>Logged In As:
-                        <br /> {props.user?.email}</p>
-                </div>
-            </div>
+            <NavBar />
             <List />
         </div>
     );
