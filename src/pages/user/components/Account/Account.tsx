d42761241dd835c5eee35e19be3930ad025315bc
Diego Vargas <diegovs0504@gmail.com>
2023-04-25 22:21:50 -0400

    Fixed positioning in Account Component

--- src/pages/user/components/Account/Account.tsx
+++ src/pages/user/components/Account/Account.tsx
@@ -10,12 +10,14 @@ const Account: React.FC<IAccountProps> = ({ }) => {
     const auth = getAuth()
 
     return (
-        <div className='flex flex-row gap-4 justify-center items-center'>
-            <div className='flex flex-col justify-center items-center'>
-                {auth.currentUser?.photoURL ? <img src={`${auth.currentUser?.photoURL}`} className='w-16 rounded-full' alt="avatar" /> : <button className='custom__button paragraph uppercase'>Upload an Image</button>}
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-                <p className='paragraph font-bold'>{auth.currentUser?.email}</p>
+        <div className='flex flex-row gap-4 justify-start items-start'>
+            <div className='flex flex-row gap-4 justify-center items-center'>
+                <div className='flex flex-col justify-center items-center'>
+                    {auth.currentUser?.photoURL ? <img src={`${auth.currentUser?.photoURL}`} className='w-16 rounded-full' alt="avatar" /> : <button className='custom__button paragraph uppercase'>Upload an Image</button>}
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <p className='paragraph font-bold'>{auth.currentUser?.email}</p>
+                </div>
             </div>
         </div>
     );
