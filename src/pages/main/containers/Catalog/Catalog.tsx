a46975a15843795383e6d4bab69d7080b156a0bf
Diego Vargas <diegovs0504@gmail.com>
2023-05-04 18:23:22 -0400

    Implemented Responsiveness on Catalog Container

--- src/pages/main/containers/Catalog/Catalog.tsx
+++ src/pages/main/containers/Catalog/Catalog.tsx
@@ -97,11 +97,11 @@ export const Catalog: FC<Props> = (props) => {
     return (
         <div>
             <div className="flex flex-row justify-between m-8">
-                <div className='flex justify-start items-center gap-12'>
-                    <h1 className='headtext uppercase text-2xl'>{props.title}</h1>
-                    <button onClick={() => handleChangeShows(0)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-2xl cursor-pointer'>Recent {props.title}</button>
-                    <button onClick={() => handleChangeShows(1)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-2xl cursor-pointer'>Trending</button>
-                    <button onClick={() => handleChangeShows(2)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-2xl cursor-pointer'>Ranked</button>
+                <div className='flex flex-row justify-start items-center gap-5 sm:gap-5 md:gap-5 lg:gap-12 xl:gap-12'>
+                    <h1 className='headtext uppercase text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>{props.title}</h1>
+                    <button onClick={() => handleChangeShows(0)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl cursor-pointer'>Recent {props.title}</button>
+                    <button onClick={() => handleChangeShows(1)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl cursor-pointer'>Trending</button>
+                    <button onClick={() => handleChangeShows(2)} className='headtext border-b border-transparent transition-all hover:-mt-2 hover:border-b hover:border-purple-600 font-normal uppercase text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl cursor-pointer'>Ranked</button>
                 </div>
                 <div className='flex justify-center items-center'>
                     <button className='custom__button border-none hover:bg-amber-400 bg-purple-700 text-white uppercase' onClick={() => handleClick()}>See More</button>
@@ -109,7 +109,7 @@ export const Catalog: FC<Props> = (props) => {
             </div>
             <div className='flex flex-row flex-wrap justify-center items-center gap-8'>
                 {isPressed ? shows.slice(0, 12).map((movie: Movie) => (
-                    <div className='flex-1 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/4 xl:basis-1/5' key={movie?.id}>
+                    <div className='flex flew-col justify-center items-center flex-1 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/4 xl:basis-1/5' key={movie?.id}>
                         <MovieCard movie={movie} mediaType={movie.name ? "tv" : "movie"} key={movie?.id} />
                     </div>
                 )) : shows.slice(0, 15).map((movie: Movie) => (
